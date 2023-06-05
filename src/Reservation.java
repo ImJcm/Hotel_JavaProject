9ab37b88f0342cf94d48ed3193e2a996791e397c
ImJcm <wjsckdals0909@naver.com>
2023-06-05 23:56:51 +0900

    호텔 예약 시스템 뼈대 수정

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,7 +1,7 @@
 import java.util.Date;
 
 public class Reservation {
-    private Customer c = new Customer();    //고객
+    private Customer customer = new Customer();    //고객
     private HotelRoom room; // 예약할 방
     private String customerName; //예약자 이름
     private String phoneNum; //예약자 번호
